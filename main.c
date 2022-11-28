b13e3ccb6119dc00eb9096a98001f7bd38b4003b
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2022-11-28 16:39:21 +0100

    working keysize arg

--- main.c
+++ main.c
@@ -15,17 +15,21 @@ static struct argp_option options[] = {
     {"generate", GENERATE_KEY, 0, 0, "Geneate a key"},
     {"output", 'o', "FILE", 0, "Output file"},
     {"keysize", 'k', "KEYSIZE", 0, "Keysize"},
+    {0}
 };
 
 error_t parse_opt (int key, char* arg, struct argp_state *state) {
     struct arguments *arguments = state->input;
-
     switch (key) {
         case GENERATE_KEY:
             arguments->mode = GENERATE;
             break;
         case 'k':
             arguments->keysize = atoi(arg);
+            if (arguments->keysize == 0) {
+                argp_failure(state, 1, 0, "Invalid keysize value");
+                exit(1);
+            }
             break;
         case '0':
             arguments->outfile = arg;
@@ -33,7 +37,7 @@ error_t parse_opt (int key, char* arg, struct argp_state *state) {
         default:
             return ARGP_ERR_UNKNOWN;
     }
-
+    return 0;
 }
 
 struct argp argp = {options, parse_opt};
@@ -50,7 +54,7 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    struct PrivateKey key = generate_keypair(32);
+    struct PrivateKey key = generate_keypair(arguments.keysize);
 
     long c = encryptc(key.pub, 'A');
     char m = decryptc(key, c);
